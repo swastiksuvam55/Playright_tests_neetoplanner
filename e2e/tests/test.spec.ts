3932b894482486931847b388106f4bb8779b7647
swastik <swastikpikun@gmail.com>
2024-04-22 22:58:38 +0530

    added tests to verify new project creation

--- e2e/tests/test.spec.ts
+++ e2e/tests/test.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, Page } from '@playwright/test';
 import { login } from '../fixtures/login';
+import { newProject } from '../fixtures/new_project';
 
 test.describe('Login Functionality', () => {
   test('should successfully login to neetoPlanner', async ({ page }: { page: Page }) => {
@@ -9,5 +10,12 @@ test.describe('Login Functionality', () => {
   });
 });
 
+test.describe('New Project creation successful', () => {
+    test('should successfully able to create a new project', async ({ page }: { page: Page }) => {
+        await login(page);
+        await newProject(page);
+        console.log('new project creation successful');
+    });
+});
 
 
