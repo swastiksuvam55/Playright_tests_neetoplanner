20b558feddbdd831424107cfbd1a222309ddeb12
swastik <swastikpikun@gmail.com>
2024-04-22 16:32:51 +0530

    explicity specified page of type Page for strict type checking

--- e2e/fixtures/login.ts
+++ e2e/fixtures/login.ts
@@ -1,15 +1,14 @@
 import { Page } from '@playwright/test';
 
-export async function login(page: Page): Promise<void> {
+export async function login(page: Page) {
   await page.goto('https://app.neetoauth.net/signups/new?redirect_uri=neetoplanner.net');
   await page.getByRole('link', { name: 'Log in' }).click();
   await page.locator('[data-test-id="neeto-auth-email-input-field"]').fill('cpts9gnqty9-planner-swastik_singh-iiit_bhubaneswar@bigbinary.com');
   await page.locator('[data-test-id="neeto-auth-login-button"]').click();
   await page.getByPlaceholder('Enter 6 digit login code').fill('123456');
-  await page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net');
+  await page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/');
   await page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/dashboard/active');
 }
-
 
 
 
--- e2e/tests/test.spec.ts
+++ e2e/tests/test.spec.ts
@@ -1,12 +1,13 @@
-import { test, expect } from '@playwright/test';
-import { login } from '../fixtures/login'; // Assuming login is exported as default from login.ts
+import { test, expect, Page } from '@playwright/test';
+import { login } from '../fixtures/login';
 
 test.describe('Login Functionality', () => {
-  test('should successfully login to neetoPlanner', async ({ page }) => {
+  test('should successfully login to neetoPlanner', async ({ page }: { page: Page }) => {
     // Step 1: Login to neetoPlanner
     await login(page);
     console.log('login successful');
   });
 });
+
 
 
