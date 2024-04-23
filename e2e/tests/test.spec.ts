4c1a19bfb5e9aa18fa6ca615b9df59bd291ce5fc
swastik <swastikpikun@gmail.com>
2024-04-23 12:50:58 +0530

    used playwright hooks to simplify the code

--- e2e/tests/test.spec.ts
+++ e2e/tests/test.spec.ts
@@ -1,19 +1,50 @@
+// import { test, expect, Page } from '@playwright/test';
+// import { login } from '../fixtures/login';
+// import { newProject } from '../fixtures/new_project';
+// import { verifySameUser } from '../fixtures/verify_assigned_user_tasks_same_user';
+
+// test.describe('Login Functionality', () => {
+//     test('should successfully login to neetoPlanner', async ({ page }: { page: Page }) => {
+//         // Step 1: Login to neetoPlanner
+//         await login(page);
+//         console.log('login successful');
+//     });
+// });
+
+// test.describe('New Project creation successful', () => {
+//     test('should successfully able to create a new project', async ({ page }: { page: Page }) => {
+//         await login(page);
+//         await newProject(page);
+//         console.log('new project creation successful');
+//     });
+// });
+
+// test.describe('verify assigned user tasks with same user', () => {
+//     test('should successfully able to assign new projects to the same user', async ({ page }: { page: Page }) => {
+//         await login(page);
+//         await verifySameUser(page);
+//         console.log('verified same user');
+//     });
+// });
+
 import { test, expect, Page } from '@playwright/test';
 import { login } from '../fixtures/login';
 import { newProject } from '../fixtures/new_project';
 import { verifySameUser } from '../fixtures/verify_assigned_user_tasks_same_user';
 
+test.beforeEach(async ({ page }: { page: Page }) => {
+    // Step 1: Login to neetoPlanner
+    await login(page);
+});
+
 test.describe('Login Functionality', () => {
     test('should successfully login to neetoPlanner', async ({ page }: { page: Page }) => {
-        // Step 1: Login to neetoPlanner
-        await login(page);
         console.log('login successful');
     });
 });
 
 test.describe('New Project creation successful', () => {
     test('should successfully able to create a new project', async ({ page }: { page: Page }) => {
-        await login(page);
         await newProject(page);
         console.log('new project creation successful');
     });
@@ -21,9 +52,7 @@ test.describe('New Project creation successful', () => {
 
 test.describe('verify assigned user tasks with same user', () => {
     test('should successfully able to assign new projects to the same user', async ({ page }: { page: Page }) => {
-        await login(page);
         await verifySameUser(page);
         console.log('verified same user');
     });
 });
-
