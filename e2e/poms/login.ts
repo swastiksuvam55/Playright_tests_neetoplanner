84da461362d01cea60afe4fb09d317c6782757cc
swastik <swastikpikun@gmail.com>
2024-04-24 01:30:53 +0530

    updated with the best practices

---
+++ e2e/poms/login.ts
@@ -0,0 +1,40 @@
+import { Page } from "@playwright/test";
+
+export default class LoginPage {
+    page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    loginAndVerifyUser = async ({
+        email,
+    }: {
+        email: string;
+
+    }): Promise<void> => {
+        await this.page.getByPlaceholder('Please enter your email').fill(email);
+        await this.page.getByRole('button', { name: 'Next' }).click();
+        await this.page.getByPlaceholder('Enter 6 digit login code').fill('123456');
+        await this.page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/');
+        await this.page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/dashboard/active');
+
+    };
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//   async loginAndVerifyUser(email: string): Promise<void> {
+//     await this.page.getByPlaceholder('Please enter your email').fill(email);
+//     await this.page.getByRole('button', { name: 'Next' }).click();
+
+//   }
