4114f3788a971d47b4195f3cc6404a201cd28090
swastik <swastikpikun@gmail.com>
2024-04-24 20:08:30 +0530

    updated for better readability

--- e2e/poms/login.ts
+++ e2e/poms/login.ts
@@ -2,9 +2,11 @@ import { Page } from "@playwright/test";
 
 export default class LoginPage {
     page: Page;
+ 
 
     constructor(page: Page) {
         this.page = page;
+      
     }
 
     loginAndVerifyUser = async ({
@@ -13,14 +15,19 @@ export default class LoginPage {
         email: string;
 
     }): Promise<void> => {
+        
         await this.page.getByPlaceholder('Please enter your email').fill(email);
         await this.page.getByRole('button', { name: 'Next' }).click();
         await this.page.getByPlaceholder('Enter 6 digit login code').fill('123456');
         await this.page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/');
         await this.page.waitForURL('https://swastik-singh-iiit-bhubaneswar.neetoplanner.net/dashboard/active');
 
+
+        
+
     };
 }
+
 
 
 
